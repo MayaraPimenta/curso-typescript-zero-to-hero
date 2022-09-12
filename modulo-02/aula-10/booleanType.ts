7aea286c756b8a99d6f5fca52a488cdeda16d43f
Mayara Pimenta <47399656+MayaraPimenta@users.noreply.github.com>
2022-09-12 14:32:16 -0300

    aula 10

---
+++ modulo-02/aula-10/booleanType.ts
@@ -0,0 +1,14 @@
+// ==> Exemplo 1
+
+let tarefaConcluida: boolean = true;
+let tarefaPendendte: boolean = false;
+console.log(tarefaConcluida, tarefaPendendte);
+
+// ==> Exemplo 2
+let concluido: boolean = false;
+
+if (concluido) {
+    console.log('tarefa concluida com sucesso')
+} else {
+    console.log('tarefa pendente')
+}
